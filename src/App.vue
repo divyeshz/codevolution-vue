e8e9d77f66230dbd48b52bb432af139aee55d035
Divyesh Mepal <divyeshm@zignuts.com>
2024-02-16 12:35:30 +0530

    Watchers

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,10 @@
 <template>
-  <h2>Fullname - {{ firstName }} {{ lastName }}</h2>
-  <h2>Computed Fullname - {{ fullName }}</h2>
-
-  <button @click="changeFullname">Change Fullname</button>
-
-
-  <button @click="items.push({
-    id: 4,
-    title: 'Keyboard',
-    price: 150,
-  })">Add item</button>
-  <h2>Computed Total - {{ total }}</h2>
-
-  <h2>Method Total {{ getTotal() }}</h2>
-
-  <input type="text" v-model="country">
-
-  <template v-for="item in items" :key="item.id">
-    <div>
-      <h2 v-if="item.price > 100"> {{ item.title }} {{ item.price }}</h2>
-    </div>
-
-  </template>
-
-  <h2 v-for="item in expensiveItems" :key="item.id"> {{ item.title }} {{ item.price }}</h2>
+  <h2>Volume Tracker (0-20)</h2>
+  <h3>Current Volume - {{ volume }}</h3>
+  <div>
+    <button @click="volume += 2">Increase</button>
+    <button @click="volume -= 2">Decrease</button>
+  </div>
 </template>
 
 <script>
@@ -31,54 +12,18 @@ export default {
   name: 'App',
   data() {
     return {
-      firstName: 'Divyesh',
-      lastName: 'Mepal',
-      items: [
-        {
-          id: 1,
-          title: 'TV',
-          price: 100,
-        },
-        {
-          id: 2,
-          title: 'Phone',
-          price: 200,
-        },
-        {
-          id: 3,
-          title: 'Laptop',
-          price: 300,
-        },
-      ],
-      country: '',
+      volume: 0
     }
   },
   methods: {
-    getTotal() {
-      console.log('getTotalMethod');
-      return this.items.reduce((total, curr) => (total = total + curr.price), 0)
-    },
-    changeFullname() {
-      this.fullName = 'Helly Patel'
-    }
   },
   computed: {
-    fullName: {
-      get() {
-        return `${this.firstName} ${this.lastName}`
-      },
-      set(value) {
-        const names = value.split(' ')
-        this.firstName = names[0]
-        this.lastName = names[1]
+  },
+  watch: {
+    volume(newValue, oldValue) {
+      if (newValue > oldValue && newValue === 16) {
+        alert('damage');
       }
-    },
-    total() {
-      console.log('getTotalComputed');
-      return this.items.reduce((total, curr) => (total = total + curr.price), 0)
-    },
-    expensiveItems() {
-      return this.items.filter(item => item.price > 100)
     }
   },
 }
