dfa32ad1ebe5130a508896d7d7f0fe96fbcf01e3
Divyesh Mepal <divyeshm@zignuts.com>
2024-02-16 11:59:51 +0530

    Computed Properties and v-for

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,15 @@
   <h2>Method Total {{ getTotal() }}</h2>
 
   <input type="text" v-model="country">
+
+  <template v-for="item in items" :key="item.id">
+    <div>
+      <h2 v-if="item.price > 100"> {{ item.title }} {{ item.price }}</h2>
+    </div>
+
+  </template>
+
+  <h2 v-for="item in expensiveItems" :key="item.id"> {{ item.title }} {{ item.price }}</h2>
 </template>
 
 <script>
@@ -54,6 +63,9 @@ export default {
       console.log('getTotalComputed');
       return this.items.reduce((total, curr) => (total = total + curr.price), 0)
     },
+    expensiveItems() {
+      return this.items.filter(item => item.price > 100)
+    }
   },
 }
 </script>
